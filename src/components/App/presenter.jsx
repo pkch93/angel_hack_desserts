3e3fde5a6d52888d5b4d0d43a529c9975950dd5e
pkch93 <star_final@naver.com>
2019-06-30 12:46:41 +0900

    feat: Nav 추가

--- src/components/App/presenter.jsx
+++ src/components/App/presenter.jsx
@@ -4,17 +4,24 @@ import { Route } from 'react-router-dom';
 import Auth from 'components/Auth';
 import Question from 'components/Question';
 import Insite from 'components/Insite';
+import Report from 'components/Report';
+import Nav from 'components/Nav';
+
 import 'reset-css';
 import 'shared/index.css'
 import './style.scss';
 
 const App = () => {
   return (
-    <div className="container">
-      <Route exact path="/" component={Auth} />
-      <Route exact path="/main" component={Question} />
-      <Route exact path="/insite" component={Insite} />
-    </div>
+    <>
+      <Nav />
+      <div className="container">
+        <Route exact path="/" component={Auth} />
+        <Route exact path="/main" component={Question} />
+        <Route exact path="/insite" component={Insite} />
+        <Route exact path="/report" component={Report} />
+      </div>
+    </>
   );
 }
 
