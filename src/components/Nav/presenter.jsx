d1bf75b0d3d7ff0f767e8d0e9d0ecc07e924125d
pkch93 <star_final@naver.com>
2019-06-30 12:27:34 +0900

    feat: Navbar

---
+++ src/components/Nav/presenter.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './style.scss';
+
+export default () => (
+    <nav className="navbar">
+        <h1 className="service-name">desserts</h1>
+        <img src="img/menu.svg" alt="hamburger menu"/>
+    </nav>
+);
